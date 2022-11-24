8742c8d194e79158b980b183ad3883bb4430db06
Muhammed-Saleh <hamada500005@gmail.com>
2022-11-24 07:02:06 -0800

    Solving Selection easy homework challenge 01

---
+++ Selection Homework Easy Challenge 01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A,B;
+	cin >> A >> B;
+
+	if (A % 2 != 0 && B % 2 != 0) {
+		cout << A * B << endl;
+	} else if (A % 2 == 0 && B % 2 == 0) {
+		cout << A / B << endl;
+	} else if (A % 2 != 0 && B % 2 == 0) {
+		cout << A + B << endl;
+	} else {
+		cout << A - B << endl;
+	}
+
+	return 0;
+}
