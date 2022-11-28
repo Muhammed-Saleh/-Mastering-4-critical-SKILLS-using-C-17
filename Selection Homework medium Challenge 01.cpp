0fa5b9d4ec1a72778c2d5489c14cf3b142d107c1
Muhammed-Saleh <hamada500005@gmail.com>
2022-11-28 09:12:28 -0800

    Solving Selection medium homework challenges

---
+++ Selection Homework medium Challenge 01.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A;
+	B = A;
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cin >> A;
+
+	if ( B < A ) {
+		B = A;
+	}
+
+	cout << B << endl;
+	return 0;
+}
---
+++ Selection Homework medium Challenge 02.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, A, B;
+	cin >> N;
+	cin >> A;
+	B = A;
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	if (N - 1 != 0) {
+		if ( B < A ) {
+			B = A;
+		}
+		N -= 1;
+		cin >> A;
+	}
+
+
+	cout << B << endl;
+	return 0;
+}
