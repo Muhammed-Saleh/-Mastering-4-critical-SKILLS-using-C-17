e984fcb25ec91666b7dcb4f88323f53260223f33
Muhammed-Saleh <hamada500005@gmail.com>
2022-12-02 00:41:13 -0800

    Solving loop easy homework challenge 03

---
+++ Loop/Loop Homework Easy Challenge 03.cpp
@@ -0,0 +1,37 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+
+	cin >> N;
+
+	int row  = N;
+	while (row >= 0) {
+		int stars_count = N;
+
+		while (stars_count >= row) {
+			cout << "*";
+			--stars_count;
+		}
+		cout << "\n";
+		row--;
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
